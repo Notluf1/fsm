199009363d2303d5b84bfb36213c6f5f1d04a197
Fulton Sheen <149178194+Notluf1@users.noreply.github.com>
2025-03-14 10:12:50 +0530

    Update temporary_link.js

--- src/elements/temporary_link.js
+++ src/elements/temporary_link.js
@@ -1,15 +1,31 @@
-function TemporaryLink(from, to) {
-	this.from = from;
-	this.to = to;
+class TemporaryLink {
+  constructor(from, to, lineColor = 'black', arrowSize = 10, lineWidth = 2) {
+    this.from = from;
+    this.to = to;
+    this.lineColor = lineColor;
+    this.arrowSize = arrowSize;
+    this.lineWidth = lineWidth;
+  }
+
+  draw(c) {
+    // Save the current context state
+    c.save();
+
+    // Set line properties
+    c.strokeStyle = this.lineColor;
+    c.lineWidth = this.lineWidth;
+
+    // Draw the line
+    c.beginPath();
+    c.moveTo(this.from.x, this.from.y);
+    c.lineTo(this.to.x, this.to.y);
+    c.stroke();
+
+    // Draw the arrowhead
+    const angle = Math.atan2(this.to.y - this.from.y, this.to.x - this.from.x);
+    drawArrow(c, this.to.x, this.to.y, angle, this.arrowSize);
+
+    // Restore the context state
+    c.restore();
+  }
 }
-
-TemporaryLink.prototype.draw = function(c) {
-	// draw the line
-	c.beginPath();
-	c.moveTo(this.to.x, this.to.y);
-	c.lineTo(this.from.x, this.from.y);
-	c.stroke();
-
-	// draw the head of the arrow
-	drawArrow(c, this.to.x, this.to.y, Math.atan2(this.to.y - this.from.y, this.to.x - this.from.x));
-};
