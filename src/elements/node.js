2ad4d6e27e96a44c49c8e88942dc3174758b5430
Fulton Sheen <149178194+Notluf1@users.noreply.github.com>
2025-03-14 10:11:49 +0530

    Update node.js

--- src/elements/node.js
+++ src/elements/node.js
@@ -1,49 +1,55 @@
-function Node(x, y) {
-	this.x = x;
-	this.y = y;
-	this.mouseOffsetX = 0;
-	this.mouseOffsetY = 0;
-	this.isAcceptState = false;
-	this.text = '';
+class Node {
+  constructor(x, y, text = '', isAcceptState = false, nodeRadius = 10, acceptStateRadius = 6) {
+    this.x = x;
+    this.y = y;
+    this.mouseOffsetX = 0;
+    this.mouseOffsetY = 0;
+    this.isAcceptState = isAcceptState;
+    this.text = text;
+    this.nodeRadius = nodeRadius;
+    this.acceptStateRadius = acceptStateRadius;
+  }
+
+  setMouseStart(x, y) {
+    this.mouseOffsetX = this.x - x;
+    this.mouseOffsetY = this.y - y;
+  }
+
+  setAnchorPoint(x, y) {
+    this.x = x + this.mouseOffsetX;
+    this.y = y + this.mouseOffsetY;
+  }
+
+  draw(c) {
+    // Draw the outer circle
+    c.beginPath();
+    c.arc(this.x, this.y, this.nodeRadius, 0, 2 * Math.PI, false);
+    c.stroke();
+
+    // Draw the text
+    drawText(c, this.text, this.x, this.y, null, selectedObject === this);
+
+    // Draw the inner circle for accept state
+    if (this.isAcceptState) {
+      c.beginPath();
+      c.arc(this.x, this.y, this.nodeRadius - this.acceptStateRadius, 0, 2 * Math.PI, false);
+      c.stroke();
+    }
+  }
+
+  closestPointOnCircle(x, y) {
+    const dx = x - this.x;
+    const dy = y - this.y;
+    const scale = Math.hypot(dx, dy);
+    return {
+      x: this.x + (dx * this.nodeRadius) / scale,
+      y: this.y + (dy * this.nodeRadius) / scale,
+    };
+  }
+
+  containsPoint(x, y) {
+    const dx = x - this.x;
+    const dy = y - this.y;
+    return dx * dx + dy * dy < this.nodeRadius * this.nodeRadius;
+  }
 }
-
-Node.prototype.setMouseStart = function(x, y) {
-	this.mouseOffsetX = this.x - x;
-	this.mouseOffsetY = this.y - y;
-};
-
-Node.prototype.setAnchorPoint = function(x, y) {
-	this.x = x + this.mouseOffsetX;
-	this.y = y + this.mouseOffsetY;
-};
-
-Node.prototype.draw = function(c) {
-	// draw the circle
-	c.beginPath();
-	c.arc(this.x, this.y, nodeRadius, 0, 2 * Math.PI, false);
-	c.stroke();
-
-	// draw the text
-	drawText(c, this.text, this.x, this.y, null, selectedObject == this);
-
-	// draw a double circle for an accept state
-	if(this.isAcceptState) {
-		c.beginPath();
-		c.arc(this.x, this.y, nodeRadius - 6, 0, 2 * Math.PI, false);
-		c.stroke();
-	}
-};
-
-Node.prototype.closestPointOnCircle = function(x, y) {
-	var dx = x - this.x;
-	var dy = y - this.y;
-	var scale = Math.sqrt(dx * dx + dy * dy);
-	return {
-		'x': this.x + dx * nodeRadius / scale,
-		'y': this.y + dy * nodeRadius / scale,
-	};
-};
-
-Node.prototype.containsPoint = function(x, y) {
-	return (x - this.x)*(x - this.x) + (y - this.y)*(y - this.y) < nodeRadius*nodeRadius;
-};
